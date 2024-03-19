918aa474b5024871bbef243d5e2ef7964fdefe58
rizwan.tyagi <rizwan.tyagi@nic.in>
2024-03-19 17:12:32 +0530

    Add project files.

---
+++ BasicAPI/Controllers/BookController.cs
@@ -0,0 +1,119 @@
+﻿using BasicAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using static BasicAPI.Controllers.BookController;
+
+namespace BasicAPI.Controllers
+{
+    public class BookController : ApiController
+    {
+        dbContext dbContext = new dbContext();
+
+        // GET api/values
+        //public dictionary<string, string> get()
+        //{
+        //    dictionary<string, string> booklist = new dictionary<string, string>();
+        //    datatable bookdt = dbcontext.getbooks();
+        //    if (bookdt != null)
+        //    {
+        //        foreach (datarow dr in bookdt.rows)
+        //        {
+        //            booklist.add(dr["name"].tostring(), dr["author"].tostring());
+        //        }
+        //    }
+        //    return booklist;
+        //}
+        public IHttpActionResult get()
+        {
+            List<Book> booklist = new List<Book>();
+            DataTable bookdt = dbContext.getBooks();
+            if (bookdt != null && bookdt.Rows.Count > 0)
+            {
+                foreach (DataRow dr in bookdt.Rows)
+                {
+                    Book book = new Book();
+                    book.Id = (Int32)dr["Id"];
+                    book.Name = dr["Name"].ToString();
+                    book.Author = dr["Author"].ToString();
+                    booklist.Add(book);
+                }
+                return Ok(booklist);
+            }
+            else
+            {
+                return BadRequest("No Book available at this time");
+            }
+          
+        }
+        // GET api/values/5
+        public IHttpActionResult Get(int id)
+        {
+            Dictionary<string, string> book = new Dictionary<string, string>();
+            DataTable bookDT = dbContext.getBookById(id);
+            if (bookDT != null && bookDT.Rows.Count > 0)
+            {
+                DataRow dr = bookDT.Rows[0];
+                book.Add(dr["Name"].ToString(), dr["Author"].ToString());
+                return Ok(book);
+            }
+            else
+            {
+                return BadRequest("No Book available on this Id");
+            }
+        }
+
+        // POST api/values
+        public IHttpActionResult Post([FromBody] Book book)
+        {
+            int affactedRow = dbContext.postBook(book);
+            if (affactedRow != 0)
+            {
+                return Ok("Book detail saved !!!");
+            }
+            else
+            {
+                return BadRequest("An error occurred while save the book detail.");
+            }
+        }
+
+        // PUT api/values/5
+        public IHttpActionResult Put([FromBody] Book book)
+        {
+            int affactedRow = dbContext.updateBook(book);
+            if (affactedRow != 0)
+            {
+                return Ok("Book detail updated !!!");
+            }
+            else
+            {
+                return BadRequest("An error occurred while save the book detail.");
+            }
+        }
+
+        // DELETE api/values/5
+        public IHttpActionResult Delete(int id)
+        {
+            int affactedRow = dbContext.deleteBook(id);
+            if (affactedRow != 0)
+            {
+                return Ok("Book detail deleted !!!");
+            }
+            else
+            {
+                return BadRequest("An error occurred while save the book detail.");
+            }
+        }
+    }
+
+    public class Book
+    {
+        public int Id;
+        public string Name;
+        public string Author;
+    }
+}
---
+++ BasicAPI/Data/dbContext.cs
@@ -0,0 +1,95 @@
+﻿using BasicAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BasicAPI.Data
+{
+    public class dbContext
+    {
+        string CS = ConfigurationManager.ConnectionStrings["connectionAPI"].ConnectionString;
+
+
+        public DataTable getBooks()
+        {
+            SqlConnection connection = new SqlConnection(CS);
+            SqlCommand cmd = new SqlCommand("select * from Book", connection) { CommandType = CommandType.Text };
+
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            dataAdapter.Fill(dt);
+            return dt;
+        }
+        public DataTable getBookById(int id)
+        {
+            SqlConnection connection = new SqlConnection(CS);
+            SqlCommand cmd = new SqlCommand("select id,Name,Author from Book where id=@id", connection) { CommandType = CommandType.Text };
+            cmd.Parameters.AddWithValue("id", id);
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            dataAdapter.Fill(dt);
+            return dt;
+        }
+        public int postBook(Book book)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(CS);
+                SqlCommand cmd = new SqlCommand("Insert into Book (Name,Author) values (@name,@author)", connection) { CommandType = CommandType.Text };
+                cmd.Parameters.AddWithValue("name", book.Name);
+                cmd.Parameters.AddWithValue("author", book.Author);
+                connection.Open();
+                int row = cmd.ExecuteNonQuery();
+                return row;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+               // throw ex;
+            }
+           
+        }
+        //
+        public int updateBook(Book book)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(CS);
+                SqlCommand cmd = new SqlCommand("update Book set Name=@name,Author=@author where id=@Id", connection) { CommandType = CommandType.Text };
+                cmd.Parameters.AddWithValue("Id", book.Id);
+                cmd.Parameters.AddWithValue("name", book.Name);
+                cmd.Parameters.AddWithValue("author", book.Author);
+                connection.Open();
+                int row = cmd.ExecuteNonQuery();
+                return row;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+                // throw ex;
+            }
+        }
+        public int deleteBook(int Id)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(CS);
+                SqlCommand cmd = new SqlCommand("delete from Book where id=@Id", connection) { CommandType = CommandType.Text };
+                cmd.Parameters.AddWithValue("Id", Id);
+                connection.Open();
+                int row = cmd.ExecuteNonQuery();
+                return row;
+            }
+            catch (Exception ex)
+            {
+                return 0;
+                // throw ex;
+            }
+        }
+        //
+    }
+}
